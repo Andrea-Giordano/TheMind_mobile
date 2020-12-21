02a76f93ab62137bd64698d0498065f305a2f26f
Andrea Giordano <andrea.giordano.inf@gmail.com>
2020-12-21 20:28:34 +0100

    added support files

---
+++ the_mind/lib/com/kdev/themind/Sizes.dart
@@ -0,0 +1,16 @@
+class Sizes {
+  Sizes._();
+
+  /// Sizes
+  static const double dialogRoundness = 20.0;
+  static const double buttonRoundness = 20.0;
+
+  static const double mainButtonWidth = 200.0;
+  static const double mainButtonHeight = 40.0;
+
+  static const double dialogButtonWidth = 200.0;
+  static const double dialogButtonHeight = 40.0;
+
+
+
+}
